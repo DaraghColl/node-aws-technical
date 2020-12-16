ce295e9bf55f87c0bfed2b21057c97e571549e9f
DaraghColl <daraghcoll@hotmail.com>
2020-12-16 02:35:40 +0000

    add vpc and subnet routes

--- routes/aws.routes.js
+++ routes/aws.routes.js
@@ -5,4 +5,12 @@ router.get('/regions', (req, res) => {
   res.json({ message: 'get all regions' });
 });
 
+router.get('/vpcs', (req, res) => {
+  res.json({ message: 'get all vpcs' });
+});
+
+router.get('/subnets', (req, res) => {
+  res.json({ message: 'get all subnets' });
+});
+
 module.exports = router;
