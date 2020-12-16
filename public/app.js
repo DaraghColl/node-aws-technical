d41c4c2b11eb94754c859d963c8307ff909bf093
DaraghColl <daraghcoll@hotmail.com>
2020-12-16 17:48:55 +0000

    create markup for binding regions, & styling for buttons and data section

--- public/app.js
+++ public/app.js
@@ -3,14 +3,37 @@ const regionsButton = document.getElementById('_get_all_regions');
 const vpcsButton = document.getElementById('_get_all_vpcs');
 const subnetsButton = document.getElementById('_get_all_subnets');
 
+const dataSection = document.getElementById('_data_section');
+
 // event listeners
 regionsButton.addEventListener('click', () => {
   fetchRegions();
 });
 
 // fetch regions
-function fetchRegions() {
+const fetchRegions = () => {
   fetch('/api/regions')
     .then((response) => response.json())
-    .then((data) => console.log(data));
-}
+    .then((data) => {
+      createRegionsHTML(data.data);
+    });
+};
+
+const createRegionsHTML = (regions) => {
+  console.log(regions);
+
+  regions.forEach((region) => {
+    let card = document.createElement('div');
+    card.classList.add('card');
+    let template;
+
+    template = `
+        <h1 class="card__heading">${region.RegionName}</h1>
+        <h2 class="card__endpoint">${region.Endpoint}</h2>
+    `;
+
+    card.innerHTML = template;
+
+    dataSection.appendChild(card);
+  });
+};
