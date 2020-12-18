699d7ad637622c646cb93d17f0931700eadc63c0
DaraghColl <daraghcoll@hotmail.com>
2020-12-18 01:22:03 +0000

    add unit tests for get vpc and get subnets

--- tests/aws.service.spec.js
+++ tests/aws.service.spec.js
@@ -7,6 +7,7 @@ const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 
 describe('aws service', () => {
+  // get regions
   it('should get all regions', (done) => {
     // stub aws describe regions
     const describeRegionsStub = sinon.stub(awsService.ec2, 'describeRegions');
@@ -34,7 +35,101 @@ describe('aws service', () => {
     awsService
       .getAllRegions()
       .then((data) => {
+        // expect data returned from promise to equal mock data
         expect(data).to.eql(mockRegionsData.Regions);
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  // get vpcs
+  it('should get all vpcs', (done) => {
+    // stub aws describe regions
+    const describeVPCsStub = sinon.stub(awsService.ec2, 'describeVpcs');
+
+    // mock regions
+    const mockVPCsData = {
+      status: 'success',
+      Vpcs: [
+        {
+          CidrBlock: 'CidrBlock',
+          DhcpOptionsId: 'dopt-*****',
+          State: 'available',
+          VpcId: 'vpc-*******',
+          OwnerId: '1234556',
+          InstanceTenancy: 'default',
+          Ipv6CidrBlockAssociationSet: [],
+          CidrBlockAssociationSet: [
+            {
+              AssociationId: 'vpc-cidr-assoc-*******',
+              CidrBlock: '*******',
+              CidrBlockState: {
+                State: 'associated',
+              },
+            },
+          ],
+          IsDefault: true,
+          Tags: [],
+        },
+      ],
+    };
+
+    describeVPCsStub.yields(null, mockVPCsData);
+
+    // call get all regions & expect return data to equal mock
+    awsService
+      .getAllVPCs()
+      .then((data) => {
+        // expect data returned from promise to equal mock data
+        expect(data).to.eql(mockVPCsData.Vpcs);
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  // get subnets
+  it('should get all Subnets by vpc id', (done) => {
+    // stub aws describe regions
+    const describeSubnetsStub = sinon.stub(awsService.ec2, 'describeSubnets');
+
+    // mock regions
+    const mockSubnetsData = {
+      status: 'success',
+      Subnets: [
+        {
+          AvailabilityZone: 'eu-west-1c',
+          AvailabilityZoneId: 'euw1-az1',
+          AvailableIpAddressCount: 4091,
+          CidrBlock: '*******',
+          DefaultForAz: true,
+          MapPublicIpOnLaunch: true,
+          MapCustomerOwnedIpOnLaunch: false,
+          State: 'available',
+          SubnetId: 'subnet-******',
+          VpcId: 'vpc-******',
+          OwnerId: '*********',
+          AssignIpv6AddressOnCreation: false,
+          Ipv6CidrBlockAssociationSet: [],
+          Tags: [],
+          SubnetArn: '*************',
+        },
+      ],
+    };
+
+    const mockVPCID = 'vpc-********';
+
+    describeSubnetsStub.yields(null, mockSubnetsData);
+
+    // call get all regions & expect return data to equal mock
+    awsService
+      .getAllSubnetsByVPC(mockVPCID)
+      .then((data) => {
+        // expect data returned from promise to equal mock data
+        expect(data).to.eql(mockSubnetsData.Subnets);
         done();
       })
       .catch((err) => {
