dad158d4a84b4fed6c8dc0e6f744517b77060d89
DaraghColl <daraghcoll@hotmail.com>
2020-12-18 01:41:46 +0000

    add cypress api test for vpcs

--- cypress/integration/api/aws.spec.js
+++ cypress/integration/api/aws.spec.js
@@ -2,7 +2,11 @@
 
 // get all regions
 describe('get all regions', () => {
-  it('should return 200 response', () => {
+  it('should retur status 200', () => {
+    cy.request('api/regions').should('have.property', 'status', 200);
+  });
+
+  it('should have application type json', () => {
     cy.request('api/regions')
       .its('headers')
       .its('content-type')
@@ -12,6 +16,26 @@ describe('get all regions', () => {
   it('should return 16 results', () => {
     cy.request('api/regions').should((response) => {
       expect(response.body.data).to.have.length(16);
+    });
+  });
+});
+
+// get all vpcs
+describe('get all vpcs', () => {
+  it('should retur status 200', () => {
+    cy.request('api/vpcs').should('have.property', 'status', 200);
+  });
+
+  it('should have application type json', () => {
+    cy.request('api/vpcs')
+      .its('headers')
+      .its('content-type')
+      .should('include', 'application/json');
+  });
+
+  it('should return 1 result', () => {
+    cy.request('api/vpcs').should((response) => {
+      expect(response.body.data).to.have.length(1);
     });
   });
 });
